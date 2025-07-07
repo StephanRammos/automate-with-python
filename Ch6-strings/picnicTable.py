12a380720bb5db88eeb14812586509fc48843a29
StephanRammos <techeagle289@gmail.com>
2025-07-07 12:28:25 -0700

    added comments

--- Ch6-strings/picnicTable.py
+++ Ch6-strings/picnicTable.py
@@ -2,6 +2,8 @@
 def printPicnic(itemsDict, leftWidth, rightWidth):
 	print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
 	for k, v in itemsDict.items():
+		#Left-align the key (like 'apples') within a field of width leftWidth (e.g. 12), and pad the rest with dots (.)
+		#convert v to a string first bc .rjust() is a string method.
 		print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
 
 picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
