90f12e8163be07af6c667e3818f1fb1be3950719
StephanRammos <techeagle289@gmail.com>
2025-07-08 10:43:50 -0700

    pq13.py initial

---
+++ Ch2-flow control/pq13.py
@@ -0,0 +1,11 @@
+#pq13.py
+#Ch2 - Flow Control #13
+# print the numbers from  1 to 10 using a for loop. Then another program using a while.
+i = 1
+while i <= 10:
+    print(i)
+    i +=1
+print("\n" )
+# now ith for loop
+for i in range(1,11):
+	print(i)
